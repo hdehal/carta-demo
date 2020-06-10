aee97b892e8b037762efaf6a3a24cf2717b78f2f
hdehal <harry@hdehal.com>
2020-06-10 18:32:08 +0000

    Updates

---
+++ precache-manifest.c006cebc31fbde2dccab8a91b74bfad7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3dd7ad7949f7bd4c6f6c8aafacdc48",
+    "url": "/carta-demo/index.html"
+  },
+  {
+    "revision": "d57a2f78a24d21b8d5c3",
+    "url": "/carta-demo/static/css/main.99628d40.chunk.css"
+  },
+  {
+    "revision": "2dc0f1743196f44245f5",
+    "url": "/carta-demo/static/js/2.240f2905.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/carta-demo/static/js/2.240f2905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d57a2f78a24d21b8d5c3",
+    "url": "/carta-demo/static/js/main.a69a05f9.chunk.js"
+  },
+  {
+    "revision": "50d22af05294a997c94d",
+    "url": "/carta-demo/static/js/runtime-main.6f09322f.js"
+  },
+  {
+    "revision": "e9609aac99addd2b5a3a04adf1c14e7c",
+    "url": "/carta-demo/static/media/logo.e9609aac.svg"
+  }
+]);
