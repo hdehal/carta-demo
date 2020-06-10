85ed6a1c29c869007f25d1eaa6cf9c7f7cb362d4
hdehal <harry@hdehal.com>
2020-06-10 21:20:44 +0000

    Styling for form and autograph component

--- src/components/form.js
+++ src/components/form.js
@@ -6,7 +6,6 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import TextField from '@material-ui/core/TextField';
 import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
-import { green } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -14,15 +13,15 @@ const useStyles = makeStyles((theme) => ({
             marginTop: theme.spacing(2),
             marginBottom: theme.spacing(2),
         },
-    },
-    button: {
-        margin: theme.spacing(1)
-    },
+    }
 }));
 const theme = createMuiTheme({
     palette: {
-        primary: green,
-    },
+        primary: {
+            main: "#25ba86",
+            contrastText: "white" //button text white instead of black
+        }
+    }
 });
 
 export default function Form() {
@@ -31,7 +30,13 @@ export default function Form() {
     return (
         <form className={classes.root} noValidate autoComplete="off">
             <FormControl fullWidth>
-                <TextField id="signatureForm" className="tooltip left" label="Signature" variant="filled" defaultValue="Your Signature" />
+                <TextField
+                    color="secondary"
+                    id="signatureForm"
+                    className="tooltip left"
+                    label="Signature"
+                    variant="filled" defaultValue="Your Signature"
+                />
             </FormControl>
 
             <Grid container alignItems="flex-start" justify="flex-end" direction="row">
