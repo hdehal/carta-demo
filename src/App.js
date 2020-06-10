30dc7c33ef85c3ee09e4d46f673d2ea9533fd6fd
hdehal <harry@hdehal.com>
2020-06-10 17:28:13 +0000

    Install React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function App() {
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <div className="paperRoot">
+        <Grid container spacing={2}>
+
+          <Paper className={classes.paper} id="paperGrant">
+          </Paper>
+
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.paper}>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.paper}>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Paper className={classes.paper} >
+            </Paper>
+          </Grid>
+
+        </Grid>
+      </div>
+
+    </BrowserRouter>
   );
 }
 
